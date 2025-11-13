6e27aabe99b1b0caac915fb536e5e4bf177511f0
Mohamed Elbhairy <mohamedelbhairy.work@gmail.com>
2025-11-13 14:14:26 +0200

    clean repo

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -1,26 +1,40 @@
+// بسم الله الرحمن الرحيم
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
-using namespace __gnu_pbds;
 // #pragma GCC optimize("Ofast")
 
 #define all(x) x.begin(), x.end()
 #define allr(x) x.rbegin(), x.rend()
 
 #define ll long long
-#define int ll
-#define vi vector< ll >
+// #define int ll
+#define vi vector< int >
 #define pl pair< int, int >
 #define vpl vector< pl >
-#define ordered_set tree< int, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update >
 
-const int dx[] = {-2, -2, 1, -1, 1, -1, 1, -1};
-const int dy[] = {1, -1, -2, -2, 1, 1, -1, -1};
+void B7airy() {
+      ios_base::sync_with_stdio(0);
+      cin.tie(0);
+      cout.tie(0);
+#ifndef ONLINE_JUDGE 
+      freopen("input.txt", "r", stdin);
+      freopen("output.txt", "w", stdout);
+#endif
+}
+const int dx[] = { 0, 0, 1, -1, 1, -1, -1, 1 };
+const int dy[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+const ll mod = 1e9 + 7, MAXN = 5e5 + 1, OO = 1e9;
 
+void solve() {
+      int n;
+      
+}
 signed main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
+      B7airy();
+      int tc = 1;
+      cin >> tc;
+      while (tc--) {
+            solve();
+      }
 }
